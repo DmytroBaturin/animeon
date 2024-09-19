934fa83625c636daa361525560e5d8aa148d28a6
Dmytro Baturin <dbaturin02@gmail.com>
2024-09-19 20:26:04 +0200

    fix mobile tab

--- src/screens/release/ui/tabs/index.tsx
+++ src/screens/release/ui/tabs/index.tsx
@@ -1,8 +1,22 @@
+'use client'
+
+import { useRef, useEffect } from 'react'
 import { TabsList, TabsTrigger } from '@/shared/components/ui/tabs'
 
 export const ReleaseTabs = () => {
+  const tabsRef = useRef(null)
+
+  useEffect(() => {
+    if (tabsRef.current) {
+      tabsRef.current.scrollTo({ left: 0, behavior: 'smooth' })
+    }
+  }, [])
+
   return (
-    <TabsList className="flex overflow-x-auto bg-black/20 overflow-y-hidden whitespace-nowrap scrollbar-hide">
+    <TabsList
+      ref={tabsRef}
+      className="flex overflow-x-auto bg-black/20 overflow-y-hidden whitespace-nowrap scrollbar-hide scroll-snap-type-x mandatory"
+    >
       <div className="flex space-x-4">
         <TabsTrigger value="account">Плеєр</TabsTrigger>
         <TabsTrigger value="trailer">Трейлер</TabsTrigger>
