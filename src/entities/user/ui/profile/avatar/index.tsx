a85f2c74d753c78eab02dc1ef77de0fb0868ff91
Dmytro Baturin <dbaturin02@gmail.com>
2024-12-03 18:12:15 +0100

    fix

--- src/entities/user/ui/profile/avatar/index.tsx
+++ src/entities/user/ui/profile/avatar/index.tsx
@@ -6,14 +6,14 @@ import {
   AvatarImage,
 } from '@/shared/components/ui/avatar'
 import { useUser } from '@/entities/user/model'
-import { memo } from 'react'
 
-export const UserAvatar = memo(() => {
+export const UserAvatar = () => {
   const { user } = useUser()
+
   return (
     <Avatar>
       {user && <AvatarImage src={`${process.env.API_HOST}/${user.avatar}`} />}
       <AvatarFallback>CN</AvatarFallback>
     </Avatar>
   )
-})
+}
