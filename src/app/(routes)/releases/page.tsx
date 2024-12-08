c68317a98903b8eae2e9d1d583ecadde81ba7102
Dmytro Baturin <dbaturin02@gmail.com>
2024-12-08 17:00:22 +0100

    cache fix

--- src/app/(routes)/releases/page.tsx
+++ src/app/(routes)/releases/page.tsx
@@ -1,18 +1,19 @@
 import { ReleasesPage } from '@/screens/releases/ui'
 import { getAnimeList } from '@/shared/api/anime/anime'
 
-export const runtime = 'edge'
 export default async function Page({
   searchParams,
 }: {
   searchParams: { [key: string]: string }
 }) {
-  const animeList = await getAnimeList(searchParams, {
-    next: {
-      revalidate: 60,
+  const animeList = await getAnimeList(
+    {
+      ...searchParams,
     },
-    cache: 'no-cache',
-  })
+    {
+      cache: 'no-cache',
+    },
+  )
 
   return <ReleasesPage animeList={animeList.data} />
 }
